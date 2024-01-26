9e15cb8a3d5eb7c909d9bca4b8c3521901aabce0
Mart Slot <martslot@gmail.com>
2024-01-26 20:49:05 +0100

    Add Dutch phone number to leader

--- keyboards/splitkb/elora/keymaps/martslot/leader.c
+++ keyboards/splitkb/elora/keymaps/martslot/leader.c
@@ -20,6 +20,8 @@ void leader_end_user(void) {
         SEND_STRING("Soeverein 78\n3817 HV\nAmersfoort");
     } else if (leader_sequence_five_keys(KC_P, KC_H, KC_O, KC_N, KC_E)) {
         SEND_STRING("+31630511860");
+    } else if (leader_sequence_four_keys(KC_F, KC_O, KC_O, KC_N)) {
+        SEND_STRING("0630511860");
     } else if (leader_sequence_four_keys(KC_C, KC_A, KC_P, KC_S)) {
         SEND_STRING(SS_TAP(X_CAPS_LOCK));
     }
