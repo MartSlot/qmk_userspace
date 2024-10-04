415e975a97ea9e36806b8f81e6496846a1706cc6
Mart Slot <martslot@gmail.com>
2024-10-04 20:26:43 +0200

    Remove quote key from main layer as it should be used from symbol layer

--- keyboards/a_dux/keymaps/martslot/keymap.c
+++ keyboards/a_dux/keymaps/martslot/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_QWERTY] = LAYOUT(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         MT_GA,   MT_AS,   MT_CD,   MT_SF,   KC_G,                      KC_H,    MT_SJ,   MT_CK,   MT_AL,   MT_GS,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  __NOP__,
                                             LT_SBSP, LT_NSPC, LT_SOSS, LT_FENT
     ),
 
