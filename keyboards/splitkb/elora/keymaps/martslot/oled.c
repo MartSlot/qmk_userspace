c3770332eeee90c2752aa24b62be2da6f3e0204b
Mart Slot <martslot@gmail.com>
2024-10-26 18:04:54 +0200

    Oled update

--- keyboards/splitkb/elora/keymaps/martslot/oled.c
+++ keyboards/splitkb/elora/keymaps/martslot/oled.c
@@ -15,7 +15,7 @@ bool oled_task_user(void) {
                 oled_write_ln_P(PSTR("Q W E R T"), false);
                 oled_write_ln_P(PSTR("A S D F G"), false);
                 oled_write_ln_P(PSTR("Z X C V B"), false);
-                oled_write_ln_P(PSTR("   Srt Spc"), false);
+                oled_write_ln_P(PSTR("   Bsp Spc"), false);
                 break;
             case LAYER_SHORTCUTS:
                 oled_write_P(PSTR("> Shortcut"), false);
@@ -23,23 +23,23 @@ bool oled_task_user(void) {
                 oled_write_ln_P(PSTR("Q W E R T"), false);
                 oled_write_ln_P(PSTR("A S D F G"), false);
                 oled_write_ln_P(PSTR("Z X C V Y"), false);
-                oled_write_ln_P(PSTR("   Srt Spc"), false);
+                oled_write_ln_P(PSTR("   Bsp Spc"), false);
                 break;
             case LAYER_NAVIGATION:
                 oled_write_P(PSTR("> Navigatn"), false);
                 oled_write_ln_P(PSTR(""), false);
                 oled_write_P(PSTR("Esc\x09 NwPw\x09"), false);
                 oled_write_P(PSTR("g a c s Re"), false);
-                oled_write_ln_P(PSTR("\x09 \x09 NtPt\x09"), false);
-                oled_write_ln_P(PSTR("   Srt Spc"), false);
+                oled_write_ln_P(PSTR("Gam NtPt\x09"), false);
+                oled_write_ln_P(PSTR("   Bsp Spc"), false);
                 break;
             case LAYER_SYMBOLS:
                 oled_write_ln_P(PSTR("> Symbols"), false);
                 oled_write_ln_P(PSTR(""), false);
-                oled_write_ln_P(PSTR("' < > \" \x09"), false);
+                oled_write_ln_P(PSTR("' < > \" `"), false);
                 oled_write_ln_P(PSTR("! - + = #"), false);
                 oled_write_ln_P(PSTR("^ / * \\ $"), false);
-                oled_write_ln_P(PSTR("   Srt Num"), false);
+                oled_write_ln_P(PSTR("   Del Num"), false);
                 break;
             case LAYER_NUMBERS:
                 oled_write_ln_P(PSTR("> Numbers"), false);
@@ -47,7 +47,7 @@ bool oled_task_user(void) {
                 oled_write_ln_P(PSTR("\x09 \x09 \x09 \x09 \x09"), false);
                 oled_write_ln_P(PSTR("1 2 3 4 5"), false);
                 oled_write_ln_P(PSTR("\x09 \x09 \x09 \x09 \x09"), false);
-                oled_write_ln_P(PSTR("   Srt Spc"), false);
+                oled_write_ln_P(PSTR("   Bsp Spc"), false);
                 break;
             case LAYER_FUNCTION:
                 oled_write_P(PSTR("> Function"), false);
@@ -55,7 +55,7 @@ bool oled_task_user(void) {
                 oled_write_ln_P(PSTR("\x09 \x09 \x09 \x09 \x09"), false);
                 oled_write_P(PSTR("F1F2F3F4F5"), false);
                 oled_write_ln_P(PSTR("\x09 \x09\x09 F11\x09"), false);
-                oled_write_ln_P(PSTR("   Num Ent"), false);
+                oled_write_ln_P(PSTR("   Bsp Spc"), false);
                 break;
             case LAYER_GAMING:
                 oled_write_ln_P(PSTR("> Game"), false);
@@ -63,15 +63,15 @@ bool oled_task_user(void) {
                 oled_write_ln_P(PSTR("e Q W E R"), false);
                 oled_write_ln_P(PSTR("s A S D G"), false);
                 oled_write_ln_P(PSTR("c Z X C V"), false);
-                oled_write_ln_P(PSTR("   Num Ent"), false);
+                oled_write_ln_P(PSTR("   Bsp Spc"), false);
                 break;
             case LAYER_GAMING_EXTRA:
                 oled_write_ln_P(PSTR("> Game+"), false);
                 oled_write_ln_P(PSTR(""), false);
                 oled_write_ln_P(PSTR("1 2 3 4 5"), false);
                 oled_write_ln_P(PSTR("6 7 8 9 0"), false);
-                oled_write_ln_P(PSTR("T G B I M"), false);
-                oled_write_ln_P(PSTR("   Num Ent"), false);
+                oled_write_ln_P(PSTR("B G T I M"), false);
+                oled_write_ln_P(PSTR("   Bsp Spc"), false);
                 break;
         }
 
@@ -130,9 +130,9 @@ bool oled_task_user(void) {
                 oled_write_ln_P(PSTR("> QWERTY"), false);
                 oled_write_ln_P(PSTR(""), false);
                 oled_write_ln_P(PSTR("Y U I O P"), false);
-                oled_write_ln_P(PSTR("H J K L :"), false);
-                oled_write_ln_P(PSTR("N M , . '"), false);
-                oled_write_ln_P(PSTR("Rep Fun"), false);
+                oled_write_ln_P(PSTR("H J K L '"), false);
+                oled_write_ln_P(PSTR("N M , . \x09"), false);
+                oled_write_ln_P(PSTR("Sft Ent"), false);
                 break;
             case LAYER_SHORTCUTS:
                 oled_write_P(PSTR("> Shortcut"), false);
@@ -140,7 +140,7 @@ bool oled_task_user(void) {
                 oled_write_ln_P(PSTR("\x09 \x09 \x09 \x09 \x09"), false);
                 oled_write_ln_P(PSTR("\x09 \x09 \x09 \x09 \x09"), false);
                 oled_write_ln_P(PSTR("\x09 \x09 \x09 \x09 \x09"), false);
-                oled_write_ln_P(PSTR("Rep Fun"), false);
+                oled_write_ln_P(PSTR("Sft Ent"), false);
                 break;
             case LAYER_NAVIGATION:
                 oled_write_P(PSTR("> Navigatn"), false);
@@ -148,15 +148,15 @@ bool oled_task_user(void) {
                 oled_write_P(PSTR("PuHo\x18 EnPd"), false);
                 oled_write_ln_P(PSTR("\x11 \x1B \x19 \x1A \x09"), false);
                 oled_write_P(PSTR("\x09 RtBsDel\x09"), false);
-                oled_write_ln_P(PSTR("Rep Fun"), false);
+                oled_write_ln_P(PSTR("Sft Ent"), false);
                 break;
             case LAYER_SYMBOLS:
                 oled_write_ln_P(PSTR("> Symbols"), false);
                 oled_write_ln_P(PSTR(""), false);
-                oled_write_ln_P(PSTR("` & [ ] %"), false);
+                oled_write_ln_P(PSTR("; & [ ] %"), false);
                 oled_write_ln_P(PSTR(": _ ( ) ?"), false);
                 oled_write_ln_P(PSTR("~ | { } @"), false);
-                oled_write_ln_P(PSTR("Rep Fun"), false);
+                oled_write_ln_P(PSTR("Sft Ent"), false);
                 break;
             case LAYER_NUMBERS:
                 oled_write_ln_P(PSTR("> Numbers"), false);
@@ -164,7 +164,7 @@ bool oled_task_user(void) {
                 oled_write_ln_P(PSTR("\x09 \x09 \x09 \x09 \x09"), false);
                 oled_write_ln_P(PSTR("6 7 8 9 0"), false);
                 oled_write_ln_P(PSTR("\x09 \x09 \x09 \x09 \x09"), false);
-                oled_write_ln_P(PSTR("Rep Fun"), false);
+                oled_write_ln_P(PSTR("Sft Ent"), false);
                 break;
             case LAYER_FUNCTION:
                 oled_write_P(PSTR("> Function"), false);
@@ -172,7 +172,7 @@ bool oled_task_user(void) {
                 oled_write_ln_P(PSTR("\x09 \x09 \x09 \x09 \x09"), false);
                 oled_write_P(PSTR("F6F7F8F9F0"), false);
                 oled_write_ln_P(PSTR("\x09 F12\x09 \x09 \x09"), false);
-                oled_write_ln_P(PSTR("Rep Fun"), false);
+                oled_write_ln_P(PSTR("Sft Ent"), false);
                 break;
             case LAYER_GAMING:
                 oled_write_ln_P(PSTR("> Game"), false);
@@ -185,7 +185,7 @@ bool oled_task_user(void) {
             case LAYER_GAMING_EXTRA:
                 oled_write_ln_P(PSTR("> Game+"), false);
                 oled_write_ln_P(PSTR(""), false);
-                oled_write_ln_P(PSTR("\x09 \x09 \x09 \x09 \x09"), false);
+                oled_write_P(PSTR("\x09 SwMpV+V-"), false);
                 oled_write_ln_P(PSTR("\x09 \x09 \x09 \x09 \x09"), false);
                 oled_write_ln_P(PSTR("\x09 \x09 \x09 \x09 \x09"), false);
                 oled_write_ln_P(PSTR("Sft Exit"), false);
