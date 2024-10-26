0d7c8167cdf2f9328fb4b2a648fb834072d1a45e
Mart Slot <martslot@gmail.com>
2024-10-26 18:10:15 +0200

    Remove autoshift function

--- keyboards/splitkb/elora/keymaps/martslot/rules.mk
+++ keyboards/splitkb/elora/keymaps/martslot/rules.mk
@@ -35,14 +35,12 @@ EXTRAKEY_ENABLE = yes      # Audio and system control
 # N-key rollover instead of 6-key rollover
 NKRO_ENABLE = yes          # Enable N-Key Rollover
 
-AUTO_SHIFT_ENABLE = yes
 COMBO_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
 LEADER_ENABLE = yes
 OS_DETECTION_ENABLE = yes
 
 INTROSPECTION_KEYMAP_C = shared/combos.c
-SRC += shared/autoshift.c
 SRC += shared/casemode.c
 SRC += shared/keymap.c
 SRC += shared/layers.c
