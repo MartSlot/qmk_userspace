d96b204b2cddb992d572d4c88ce492a81151fdca
Mart Slot <martslot@gmail.com>
2025-09-06 22:42:19 +0200

    Remove numword combo and unused combos

--- shared/combos.c
+++ shared/combos.c
@@ -1,26 +1,18 @@
 #include "combos.h"
 
-enum combos { COMBO_REBOOT_LEFT, COMBO_REBOOT_RIGHT, COMBO_DELETE, COMBO_BACKSPACE, COMBO_TAB, COMBO_SHIFT_TAB, COMBO_ENTER, COMBO_NUM_WORD };
+enum combos { COMBO_REBOOT_LEFT, COMBO_REBOOT_RIGHT, COMBO_TAB, COMBO_SHIFT_TAB };
 
 const uint16_t PROGMEM combo_reboot_left[]  = {KC_1, KC_2, KC_Q, COMBO_END};
 const uint16_t PROGMEM combo_reboot_right[] = {KC_3, KC_4, KC_P, COMBO_END};
-const uint16_t PROGMEM combo_delete[]       = {KC_L, KC_SCLN, COMBO_END};
-const uint16_t PROGMEM combo_backspace[]    = {KC_K, KC_L, COMBO_END};
 const uint16_t PROGMEM combo_tab[]          = {KC_D, KC_F, COMBO_END};
 const uint16_t PROGMEM combo_shift_tab[]    = {KC_S, KC_D, COMBO_END};
-const uint16_t PROGMEM combo_enter[]        = {KC_J, KC_K, COMBO_END};
-const uint16_t PROGMEM combo_num_word[]     = {KC_2, KC_3, COMBO_END};
 
 // clang-format off
 combo_t key_combos[] = {
     [COMBO_REBOOT_LEFT]          = COMBO(combo_reboot_left, QK_BOOTLOADER),
     [COMBO_REBOOT_RIGHT]         = COMBO(combo_reboot_right, QK_BOOTLOADER),
-    [COMBO_DELETE]               = COMBO(combo_delete, CO_DELETE),
-    [COMBO_BACKSPACE]            = COMBO(combo_backspace, CO_BACKSPACE),
     [COMBO_TAB]                  = COMBO(combo_tab, CO_TAB),
     [COMBO_SHIFT_TAB]            = COMBO(combo_shift_tab, S(CO_SHIFT_TAB)),
-    [COMBO_ENTER]                = COMBO(combo_enter, CO_ENTER),
-    [COMBO_NUM_WORD]             = COMBO(combo_num_word, UC_NUM_WORD),
 };
 // clang-format on
 
@@ -35,10 +27,6 @@ bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode
 }
 
 bool get_combo_must_tap(uint16_t combo_index, combo_t *combo) {
-    switch (combo_index) {
-        case COMBO_NUM_WORD:
-            return true;
-    }
     if (combo->keycode == last_keycode && timer_elapsed(last_event_time) < 1000) {
         return false;
     }
@@ -50,20 +38,6 @@ bool process_record_combo(uint16_t keycode, keyrecord_t *record) {
     last_event_time = record->event.time;
 
     switch (keycode) {
-        case CO_DELETE:
-            //            if (record->event.pressed) {
-            //                register_code(KC_DELETE);
-            //            } else {
-            //                unregister_code(KC_DELETE);
-            //            }
-            break;
-        case CO_BACKSPACE:
-            //            if (record->event.pressed) {
-            //                register_code(KC_BACKSPACE);
-            //            } else {
-            //                unregister_code(KC_BACKSPACE);
-            //            }
-            break;
         case CO_TAB:
             if (record->event.pressed) {
                 register_code(KC_TAB);
@@ -81,13 +55,6 @@ bool process_record_combo(uint16_t keycode, keyrecord_t *record) {
                 unregister_code(KC_TAB);
                 unregister_weak_mods(MOD_LSFT);
             }
-            break;
-        case CO_ENTER:
-            //            if (record->event.pressed) {
-            //                register_code(KC_ENTER);
-            //            } else {
-            //                unregister_code(KC_ENTER);
-            //            }
             break;
         default:
             break;
